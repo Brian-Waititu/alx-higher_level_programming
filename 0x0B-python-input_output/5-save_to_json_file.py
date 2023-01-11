b2f336197ec12ea878ddcb41bc2ba9c3c91c9aa6
Brayo-The-Great <111040819+Brayo-The-Great@users.noreply.github.com>
2023-01-11 21:19:28 +0300

    Update 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,20 @@
-o
+#!/usr/bin/python3
+""" Module that contains a function that returns the JSON
+representation of an object
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """ Function that returns the JSON representation of an object
+    Args:
+        my_obj: object
+    Raises:
+        Exception: when the object can't be encoded
+    """
+    return json.dumps(my_obj)
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+P
