f9dc8240433b85451a6ae726695b8334dbd006b9
Brayo-The-Great <111040819+Brayo-The-Great@users.noreply.github.com>
2023-01-11 21:41:55 +0300

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-""" Module that returns the dictionary description with a simple
-data structure for a JSON serialization of an object
+""" Module that adds all arguments to a Python list, and then
+save them to a file
 """
+import sys
+import os.path
 
 
-def class_to_json(obj):
-    """ Function that retuns the dictionary description of an obj """
+save_file = __import__('7-save_to_json_file').save_to_json_file
+load_file = __import__('8-load_from_json_file').load_from_json_file
 
-    res = {}
-    if hasattr(obj, "__dict__"):
-        res = obj.__dict__.copy()
-    return res
+my_list = []
+if os.path.exists("add_item.json"):
+    my_list = load_file("add_item.json")
+
+for arg in sys.argv[1:]:
+    my_list.append(arg)
+
+save_file(my_list, "add_item.json")
